40e0f355e13e2e31844f6adabb9c61aea628181a
crishernandezmaps <crishmill@gmail.com>
2017-04-07 11:47:48 -0300

    new data

---
+++ FADEU/fadeu.js
@@ -0,0 +1,77 @@
+const request = require("request");
+const fs = require('fs');
+
+function lux(ciudad) {
+  // URL llamando la API de Smart Citizen https://smartcitizen.me
+  const url = "https://api.smartcitizen.me/v0/devices/world_map";
+
+  // Opciones para realizar el 'PARSE' de los datos JSON.
+  const options = {url:url, json:true};
+
+  // Llamado a la API via http request.
+  request(options, function (error, response, body) {
+    let ids = [];
+    for (let i = 0; i < body.length; i++) {
+      let id = body[i].id;
+      let city = body[i].city;
+      let country = body[i].country_code;
+
+      if (city == ciudad) {
+        ids.push(id)
+      };
+
+    };
+
+    // Iteramos en la lista de los sensores disponibles para la ciudad seleccionada.
+    for (let i = 0; i < ids.length; i++) {
+      let url_device = 'https://api.smartcitizen.me/v0/devices/' + ids[i];
+      let optionsB = {url:url_device, json:true};
+      request(optionsB, function (error, response, body) {
+        let id_device = body.id;
+        let cit = body.data.location.city;
+        let con = body.data.location.country;
+        let lat = body.data.location.latitude;
+        let lon = body.data.location.longitude;
+        let sensors = body.data.sensors;
+        for (let i = 0; i < sensors.length; i++) {
+          let light = sensors[i].id;
+          let raw_value = sensors[i].raw_value;
+          let prev_raw_value = sensors[i].prev_raw_value;
+
+          // Le pedimos a los datos que nos entreguen la información sobre los sensores de luz.
+          if (light === 14) {
+            let avg = (parseInt(raw_value) + parseInt(prev_raw_value))/2
+            let result = con + ',' + cit + ',' + id_device + ',' + avg + ',' + lat + ',' + lon + ',' + url_device + '\n';
+
+            // Escribimos documento donde guardaremos resultados
+            let headers = 'pais' + ',' + 'ciudad' + ',' + 'id' + ',' + 'avgLux' + ',' +  'latitude' +  ',' +  'longitude' +  ',' + 'url' + '\n'; // Nombre columnas
+            let n = 'data' + '/' + 'resultados' + '.csv'; // path
+
+            fs.readFile(n, 'utf8', function (err,data) {
+              if (err) {
+                fs.writeFile(n, headers, function (err) {
+                    if (err) throw err;
+                });
+                fs.appendFile(n, result, (err) => {
+                    if (err) throw err;
+                });
+              } else {
+                fs.appendFile(n, result, (err) => {
+                    if (err) throw err;
+                });
+              }
+
+            });
+
+            // Mostramos los resultados de la búsqueda en la consola.
+            console.log(result);
+
+          };
+
+        };
+      });
+    };
+  });
+};
+
+const busqueda = lux('Buenos Aires'); // Modifica la Ciudad por una de tu interé. Ejemplo: 'Buenos Aires'.
---
+++ FADEU/instrucciones.md
@@ -0,0 +1,27 @@
+# Taller OCTAL - FADEU
+
+## Presentación
+El siguiente taller busca la realización de un ejercicio práctico de ciencia ciudadana, enfocado al conocimiento del impacto de la polución lumínica en Chile e internacionalmente.
+
+CSV to GeoJson: http://www.convertcsv.com/csv-to-geojson.htm
+npm install -g csv2geojson ::: https://github.com/mapbox/csv2geojson 
+
+
+## Escala Lux
+|Illuminance (lux)|Surfaces illuminated by|
+|:------------- |:-------------|
+|0.0001|Moonless, overcast night sky (starlight)|
+|0.002|Moonless clear night sky with airglow|
+|0.05–0.36|Full moon on a clear night|
+|3.4|Dark limit of civil twilight under a clear sky|
+|20–50|Public areas with dark surroundings|
+|50|Family living room lights (Australia, 1998)|
+|80|Office building hallway/toilet lighting|
+|100|Very dark overcast day|
+|320–500|Office lighting|
+|400|Sunrise or sunset on a clear day.|
+|1000|Overcast day; typical TV studio lighting|
+|10,000–25,000|Full daylight (not direct sun)|
+|32,000–100,000|Direct sunlight|
+
+*fuente: [wikipedia](https://en.wikipedia.org/wiki/Lux)*
