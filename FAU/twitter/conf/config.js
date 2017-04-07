a4322c945ac058af2f9f423417594114fa563079
Cris Hernandez <crishmill@gmail.com>
2017-04-07 11:52:31 -0300

    update config

--- FAU/twitter/conf/config.js
+++ FAU/twitter/conf/config.js
@@ -1,13 +1,6 @@
 module.exports = {
-  consumer_key:         'sVbuW6j8auJNZcd7nk5eoQMEr',
-  consumer_secret:      'IjgXjpGEBS7aos2hUmdnNhhcy7DMXovB4GUq7GilcBNWnXIFEP',
-  access_token:         '1081325768-zOTJ9FFU8zkOKeu6R7l9fKZmxRLcEUevUIGmldy',
-  access_token_secret:  'AROV2pzAZOn3iZX2AGcmjrGat574J9jxzGlEtOjAJKbW5',
+  consumer_key:         '',
+  consumer_secret:      '',
+  access_token:         '',
+  access_token_secret:  '',
 }
-
-// module.exports = {
-//   consumer_key:         '',
-//   consumer_secret:      '',
-//   access_token:         '',
-//   access_token_secret:  '',
-// }
